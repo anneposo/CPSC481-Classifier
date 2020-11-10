59ccea8e4a6f2787de85adc1478d4aa407c95f9d
anneposo <43505612+anneposo@users.noreply.github.com>
2020-11-10 12:38:05 -0800

    Add files via upload

---
+++ code/app.js
@@ -0,0 +1,12 @@
+// Selectors
+const formInput = document.querySelector('.form-input');
+const formButton = document.querySelector('.form-button');
+const formQuestions = document.querySelector('.form-questions');
+
+// Event Listeners
+formButton.addEventListener('click', submitForm);
+
+// Functions
+function submitForm(event) {
+    
+}
